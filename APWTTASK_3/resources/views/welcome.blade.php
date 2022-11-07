8afd326e0a2528b8d8c62654b09333bab48a469e
alaminrifat <alaminrifat71@gmail.com>
2022-11-07 21:07:17 +0600

    Lab task 3 Complete

---
+++ APWTTASK_3/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.main')
+@section('content')
+
+
+
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        Group Members-
+ </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">1.Muhammad Al-Amin Rifat [20-42953-1]</li>
+    </ul>
+  </div>
+  
+
+
+
+
+@endsection
